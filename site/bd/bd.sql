f599f4291208626851228e05dbccf9b5f9a9be76
jeandrei <jeandreiwalter@gmail.com>
2022-04-26 00:37:37 +0000

    aula 20

--- site/bd/bd.sql
+++ site/bd/bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Tempo de geração: 23-Abr-2022 às 01:34
+-- Tempo de geração: 26-Abr-2022 às 00:29
 -- Versão do servidor: 5.7.37
 -- versão do PHP: 8.0.15
 
@@ -35,6 +35,14 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Extraindo dados da tabela `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `body`, `create_date`) VALUES
+(1, 'My Post One', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2022-04-26 00:29:19'),
+(2, 'My Post Two', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage', '2022-04-26 00:29:19');
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -52,7 +60,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT de tabela `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- site/classes/Database.php
+++ site/classes/Database.php
@@ -28,6 +28,39 @@ class Database {
            $this->error = $e->getMessage();
         }
     }
+
+    public function query($query){
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    public function bind($param, $value, $type = null){
+        if(iss_null($type)){
+            switch(true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_boll($value):
+                    $type = PDO::PARAM_BOLL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute(){
+        return $this->stmt->execute();
+    }
+
+    public function resultSet(){
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
 }
 
 ?>
